51c0b98b6998d187f1013516328e8b0ee32e2c9f
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2024-08-12 12:46:39 +0200

    update docker install script

--- p3/install_k3s_docker.sh
+++ p3/install_k3s_docker.sh
@@ -1,6 +1,25 @@
-sudo apt install -y vim
-sudo apt install -y docker
-sudo apt install -y curl
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+sudo apt-get install -y vim
+sudo apt-get install -y curl
+
+# Add Docker's official GPG key:
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
 
 sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644" sh -
 
--- p3/uninstall.sh
+++ p3/uninstall.sh
@@ -1,5 +1,9 @@
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+
 sudo apt remove -y vim
-sudo apt remove -y docker
 sudo apt remove -y curl
 
 sudo /usr/local/bin/k3s-uninstall.sh
