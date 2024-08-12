291027ccaea9e4c2582fef946b4c41a5a40a5403
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2024-08-12 12:51:04 +0200

    update scripts p3

--- p3/install_k3s_docker.sh
+++ p3/install_k3s_docker.sh
@@ -2,10 +2,9 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 
 sudo apt-get update
 sudo apt-get install -y vim
-sudo apt-get install -y curl
 
 # Add Docker's official GPG key:
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
