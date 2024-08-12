8d35da5bb1658b42a6d1426da8358216af21de89
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2024-08-12 12:28:16 +0200

    add cubeconfigmode to k3s install

--- p3/install_k3s_docker.sh
+++ p3/install_k3s_docker.sh
@@ -2,7 +2,7 @@ sudo apt install -y vim
 sudo apt install -y docker
 sudo apt install -y curl
 
-sudo curl -sfL https://get.k3s.io | sh -
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644" sh -
 
 sleep 10
 
